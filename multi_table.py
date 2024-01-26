8a30a3783410fd4bbb13ba31c93e8df9b062ed07
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 21:14:34 +0800

    project done

--- multi_table.py
+++ multi_table.py
@@ -3,6 +3,10 @@
 # Pseudocode
 
 # Outer loop to iterate over numbers from 1 to 10
+for i in range(1, 11):
 # Inner loop to iterate over numbers from 1 to 10 for each i
+    for j in range(1, 11):
 # Print the product of i and j with a space at the end
+        print(i * j, end=" ")
 # Print a newline character to move to the next row
+    print("\t\t")
