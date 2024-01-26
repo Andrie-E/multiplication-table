6d0b25eb3a9701df12444b225711543f509b7bb0
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 21:06:23 +0800

    pseudocode

---
+++ multi_table.py
@@ -0,0 +1,8 @@
+# Print multiplication table from 1 to 10
+
+# Pseudocode
+
+# Outer loop to iterate over numbers from 1 to 10
+# Inner loop to iterate over numbers from 1 to 10 for each i
+# Print the product of i and j with a space at the end
+# Print a newline character to move to the next row
